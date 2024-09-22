5d40219b6d50628d68759d8eede9dc4ea44a6ea3
Caio Gandra <caio.gandra@freestar.io>
2024-09-22 20:36:46 -0300

    Added trusted jobs

---
+++ jobs/accelerometer_landing_to_trusted.py
@@ -0,0 +1,30 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Script generated for node Customer Trusted
+CustomerTrusted_node1726872020356 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lh/customer/trusted/"], "recurse": True}, transformation_ctx="CustomerTrusted_node1726872020356")
+
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1726872015446 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lh/accelerometer/landing/"], "recurse": True}, transformation_ctx="AccelerometerLanding_node1726872015446")
+
+# Script generated for node Filter Customer Trusted
+FilterCustomerTrusted_node1726872074939 = Join.apply(frame1=AccelerometerLanding_node1726872015446, frame2=CustomerTrusted_node1726872020356, keys1=["user"], keys2=["email"], transformation_ctx="FilterCustomerTrusted_node1726872074939")
+
+# Script generated for node Drop Customer Fields
+DropCustomerFields_node1727041453530 = ApplyMapping.apply(frame=FilterCustomerTrusted_node1726872074939, mappings=[("user", "string", "user", "string"), ("timestamp", "bigint", "timestamp", "bigint"), ("x", "double", "x", "float"), ("y", "double", "y", "float"), ("z", "double", "z", "float"), ("customername", "string", "customername", "string"), ("birthday", "string", "birthday", "string"), ("serialnumber", "string", "serialnumber", "string"), ("registrationdate", "bigint", "registrationdate", "bigint"), ("lastupdatedate", "bigint", "lastupdatedate", "bigint"), ("sharewithresearchasofdate", "bigint", "sharewithresearchasofdate", "bigint"), ("sharewithpublicasofdate", "bigint", "sharewithpublicasofdate", "bigint"), ("sharewithfriendsasofdate", "bigint", "sharewithfriendsasofdate", "bigint")], transformation_ctx="DropCustomerFields_node1727041453530")
+
+# Script generated for node Accelerometer Trusted
+AccelerometerTrusted_node1726873247382 = glueContext.write_dynamic_frame.from_options(frame=DropCustomerFields_node1727041453530, connection_type="s3", format="json", connection_options={"path": "s3://stedi-lh/accelerometer/trusted/", "partitionKeys": []}, transformation_ctx="AccelerometerTrusted_node1726873247382")
+
+job.commit()
---
+++ jobs/customer_landing_to_trusted.py
@@ -0,0 +1,34 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Script generated for node Customer Landing
+CustomerLanding_node1727038776755 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lh/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1727038776755")
+
+# Script generated for node Privacy Filter
+SqlQuery0 = '''
+select * from myDataSource where shareWithResearchAsOfDate is not null
+
+'''
+PrivacyFilter_node1727039079702 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":CustomerLanding_node1727038776755}, transformation_ctx = "PrivacyFilter_node1727039079702")
+
+# Script generated for node Customer Trusted
+CustomerTrusted_node1727039126297 = glueContext.write_dynamic_frame.from_options(frame=PrivacyFilter_node1727039079702, connection_type="s3", format="json", connection_options={"path": "s3://stedi-lh/customer/trusted/", "partitionKeys": []}, transformation_ctx="CustomerTrusted_node1727039126297")
+
+job.commit()
